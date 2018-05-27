75ef2596294252052062056489fb922b3f2e1765
admin <admin>
2018-05-27 03:00:13 +0200

    customize

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
 
 import { Account, LoginModalService, Principal } from '../shared';
+import {CandidatService, Candidat} from '../entities/candidat';
 
 @Component({
     selector: 'jhi-home',
@@ -14,22 +16,35 @@ import { Account, LoginModalService, Principal } from '../shared';
 })
 export class HomeComponent implements OnInit {
     account: Account;
+    candidats: Candidat[];
     modalRef: NgbModalRef;
 
     constructor(
         private principal: Principal,
         private loginModalService: LoginModalService,
+        private candidatService: CandidatService,
         private eventManager: JhiEventManager
     ) {
     }
 
     ngOnInit() {
+        this.loadAllCandidats();
         this.principal.identity().then((account) => {
             this.account = account;
         });
         this.registerAuthenticationSuccess();
     }
+    loadAllCandidats() {
+        this.candidatService.query().subscribe(
+            (res: HttpResponse<Candidat[]>) => {
+                this.candidats = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
 
+    private onError(error) {
+    }
     registerAuthenticationSuccess() {
         this.eventManager.subscribe('authenticationSuccess', (message) => {
             this.principal.identity().then((account) => {
